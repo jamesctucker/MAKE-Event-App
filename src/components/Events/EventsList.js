46b93296f2fc0b2c8ea926e07e08bdffb32abb02
James Tucker <jamesctucker@protonmail.com>
2019-02-13 14:38:26 -0600

    events from database are successfully displaying on the event page

---
+++ src/components/Events/EventsList.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+// import PropTypes from 'prop-types';
+// import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+// import Link from '@material-ui/core/Link';
+// import Chip from '@material-ui/core/Chip';
+// import Tooltip from '@material-ui/core/Tooltip';
+// import { CardActionArea } from '@material-ui/core';
+
+class EventsList extends Component {
+
+    componentDidMount() {
+        const action = { type: 'FETCH_EVENTS' }
+        this.props.dispatch(action);
+    }
+    render() {
+        return (
+            <Card>
+                <CardContent>
+                    <Typography>{this.props.result.event_start_date}</Typography>
+                    <Typography>{this.props.result.event_name}</Typography>
+                    <Typography>{this.props.result.event_city}</Typography>
+                    <Typography>{this.props.result.event_description}</Typography>
+                </CardContent>
+                <CardActions>
+                    <Button size="small">Learn More</Button>
+                </CardActions>
+            </Card>
+        )
+    }
+}
+
+const mapStoreToProps = reduxStore => ({
+    reduxStore,
+});
+
+export default connect(mapStoreToProps)(EventsList);
