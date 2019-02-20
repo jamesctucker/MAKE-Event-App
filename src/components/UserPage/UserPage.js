ed399167e604e8cde8e2b8a15b468f99aa6bd29e
James Tucker <jamesctucker@protonmail.com>
2019-02-20 17:13:50 -0600

    styled user account page

--- src/components/UserPage/UserPage.js
+++ src/components/UserPage/UserPage.js
@@ -1,36 +1,79 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import TextField from '@material-ui/core/TextField';
+import Input from '@material-ui/core/Input';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import InputLabel from '@material-ui/core/InputLabel';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
+import moment from 'moment'
+import './UserPage.css';
+
 
 // this could also be written with destructuring parameters as:
 // const UserPage = ({ user }) => (
 // and then instead of `props.user.username` you could use `user.username`
 const UserPage = (props) => (
   <div>
-    <h1>Your Account</h1>
-    <h3 id="welcome">
-      Welcome, {props.user.username}!
-    </h3>
-    <p>Your ID is: {props.user.id}</p>
-    <TextField variant="outlined" label="Name" value={props.user.name}></TextField>
-    <TextField variant="outlined" label="Birthdate" value={props.user.dob}></TextField>
-    <TextField variant="outlined" label="Email" value={props.user.email}></TextField>
-    <TextField variant="outlined" label="Phone" value={props.user.phone}></TextField>
-    <TextField variant="outlined" label="Hometown" value={props.user.hometown}></TextField>
-    <TextField variant="outlined" label="Country" value={props.user.country_id}></TextField>
-    <TextField variant="outlined" label="Gender" value={props.user.gender_id}></TextField>
-    <TextField variant="outlined" label="Food Preferences" value={props.user.food_preferences}></TextField>
-    <TextField variant="outlined" label="Preferred Transportation" value={props.user.preferred_transportation}></TextField>
-
-
+    <Typography variant="h5">
+      Your Account Information
+    </Typography>
+    <Paper id="paper" elevation={3}>
+      <Typography variant="h6">
+        Welcome, {props.user.username}!
+    </Typography>
+      <InputLabel id="text-field" variant="outlined">
+        Name:
+      </InputLabel>
+      <Input id="text-field" label="Name" placeholder={props.user.name}></Input>
+      <InputLabel id="text-field" variant="outlined">
+        Birthdate:
+      </InputLabel>
+      <Input id="text-field" variant="outlined" label="Birthdate" value={moment(props.user.dob).format("MMMM Do, YYYY")}></Input>
+      <InputLabel id="text-field" variant="outlined">
+        Email:
+      </InputLabel>
+      <Input id="text-field" variant="outlined" label="Email" value={props.user.email}></Input>
+      <InputLabel id="text-field" variant="outlined">
+        Phone:
+      </InputLabel>
+      <Input id="text-field" variant="outlined" label="Phone" value={props.user.phone}></Input>
+      <InputLabel id="text-field" variant="outlined">
+        Hometown:
+      </InputLabel>
+      <Input id="text-field" variant="outlined" label="Hometown" value={props.user.hometown}></Input>
+      <InputLabel id="text-field" variant="outlined">
+        Country:
+      </InputLabel>
+      <Input id="text-field" variant="outlined" label="Country" value={props.user.country_id}></Input>
+      <InputLabel id="text-field" variant="outlined">
+        Gender:
+      </InputLabel>
+      <Input id="text-field" variant="outlined" label="Gender" value={props.user.gender_id}></Input>
+      <InputLabel id="text-field" variant="outlined">
+        Food Preferences:
+      </InputLabel>
+      <Input id="text-field" variant="outlined" label="Food Preferences" value={props.user.food_preferences}></Input>
+      <InputLabel id="text-field" variant="outlined">
+        Preferred Transportation:
+      </InputLabel>
+      <Input id="text-field" variant="outlined" label="Preferred Transportation" value={props.user.preferred_transportation}></Input>
+      <div>
+        <Button variant='contained'>Update Profile</Button>
+      </div>
+    </Paper>
     <div>
-      <Button variant='contained'>Update Profile</Button>
+      <Typography variant="h5">
+        Your Events
+          </Typography>
+      <Paper id="paper" elevation={3}>
+        <Typography variant="body1">
+          Events will go here as cards or list?
+            </Typography>
+      </Paper>
     </div>
-  </div>
+  </div >
 );
 
 // Instead of taking everything from state, we just want the user info.
