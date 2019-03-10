ab6fc08c1a55efd019b20405ade1681382bde1c0
James Tucker <jamesctucker@protonmail.com>
2019-03-10 15:04:34 -0500

    updated heroku build in package json

--- src/components/FileUpload/TestFileUpload.js
+++ src/components/FileUpload/TestFileUpload.js
@@ -5,11 +5,6 @@ import { FilePond } from 'react-filepond';
 // Import FilePond styles
 import 'filepond/dist/filepond.min.css';
 
-// Import the Image EXIF Orientation and Image Preview plugins
-// Note: These need to be installed separately
-// import FilePondPluginImageExifOrientation from 'filepond-plugin-image-exif-orientation';
-// import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
-// import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css';
 
 // Register the plugins
 const AWS = require('aws-sdk');
